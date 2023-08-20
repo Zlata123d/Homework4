cb43f5eecc0d61319f4702beac6e5473dcfca174
123 <zlata18_00@mail.ru>
2023-08-20 17:05:04 +0300

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
 
     public static void task1() {
@@ -39,4 +40,36 @@ public class Main {
             System.out.println("Превышения скорости нет");
         }
     }
-}
+    public static void task4(){
+        System.out.println("Задача 4");
+        int age=2;
+        if (age <=2){
+            System.out.println("Тебе пора спать");}
+            int age2 =2;
+            if ( age2 ==2 || age2 <=6);{
+                System.out.println("Тебе пора в садик");}
+
+        int age3=10;
+        if( age3 ==7 || age3 <=18 ){
+            System.out.println("Тебе надо ходить в школу");}
+// после школы не выводит данные в консоль, но не выдаёт ошибки
+        int age4=20;
+        if( age >18 &&  age4<=24){
+            System.out.println("Твоё место в университете");}
+
+        int age5= 25;
+        if (age5 <=24){
+            System.out.println("Тебе пора ходить на работу");}
+
+        int age6=76;
+        if (age6 <=60){
+            System.out.println("Ты можешь отдохнуть");}
+
+        }
+        }
+
+
+
+
+
+
