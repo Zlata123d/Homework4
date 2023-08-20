5ca520f2a3d3f525434c796f905a8233e515e5cc
123 <zlata18_00@mail.ru>
2023-08-20 15:41:19 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+int age=18;
+if (age >=17){
+    System.out.println("Возраст совершеннолетия еще не наступил и нужно немного подождать");
+}
+
+    }
+}
